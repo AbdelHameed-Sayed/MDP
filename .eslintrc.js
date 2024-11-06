b0414dee3eb545a5e3cad60a281ce1534d6a976b
AbdelHameed Sayed <abali@aaib.com>
2024-11-06 20:03:37 +0200

    feat: eslint rules - husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,70 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['import', 'react-native'],
+  rules: {
+    'no-console': 'error',
+    'no-empty': ['error', {allowEmptyCatch: true}],
+    'import/no-unresolved': 'error',
+    'import/export': 'error',
+    'import/no-deprecated': 'error',
+    'import/named': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@(atoms|molecules|organisms|screens)/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'no-return-await': 'error',
+    'no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+      },
+    ],
+    'no-duplicate-imports': 'error',
+    'array-callback-return': 'error',
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/ignore': ['node_modules'],
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  ignorePatterns: [
+    '*.test.js',
+    '*.test.ts',
+    '*.test.tsx',
+    '**/__tests__/**',
+    '**/tests/**',
+  ],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'babel-plugin-module-resolver',
+      {
+        alias: {
+          '@assets': './src/assets',
+          '@screens': './src/screens',
+          '@atoms': './src/components/atoms',
+          '@molecules': './src/components/molecules',
+          '@organisms': './src/components/organisms',
+          '@modals': './src/components/modals',
+          '@templates': './src/components/templates',
+          '@navigation': './src/navigation',
+          '@utils': './src/utils',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
